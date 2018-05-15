f18377b5b4d0a5b8f8b2023bc1b8ca3c567202a7
el-davo <david.ahern26@gmail.com>
2018-05-15 17:34:58 +0100

    Requires user id

--- server/models/upload.js
+++ server/models/upload.js
@@ -4,13 +4,14 @@ let {getFileFromRequest} = require('../utils/upload');
 let {uploadFileToS3} = require('../utils/s3');
 
 module.exports = Upload => {
-  Upload.upload = (req, next) => {
+  Upload.upload = (userId, req, next) => {
     let uploadedFile = Upload.app.models.UploadedFile;
 
     getFileFromRequest(req)
       .then((file) => uploadFileToS3(file))
       .then((file) => {
         uploadedFile.create({
+          userId,
           link: file.Location,
           etag: file.ETag,
           bucket: file.Bucket,
@@ -28,6 +29,7 @@ module.exports = Upload => {
 
   Upload.remoteMethod('upload', {
     accepts: [
+      {arg: 'userId', type: 'string', required: true},
       {arg: 'req', type: 'object', http: {source: 'req'}}
     ],
     returns: {root: true, type: 'object'},
